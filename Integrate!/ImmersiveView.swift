206b24e11432e206a27793a51b3320730a0619b0
Om Raheja <omr@rslp.org>
2024-07-04 17:12:08 -0400

    added more nonfunctioning collision stuff

--- Integrate!/ImmersiveView.swift
+++ Integrate!/ImmersiveView.swift
@@ -5,11 +5,13 @@
 //  Created by Om Raheja on 6/25/24.
 //
 
+import Combine
 import SwiftUI
 import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
+    var cancellables: Set<AnyCancellable> = []
     var body: some View {
         RealityView { content in
             // Make it immserive
@@ -27,34 +29,43 @@ struct ImmersiveView: View {
                 let box = ModelEntity(mesh: .generateBox(size: 0.5), materials: [SimpleMaterial(color: .red, isMetallic: false)])
                 
                 immersiveContentEntity.addChild(box)
-                box.transform.translation = [0, 0, -3]
+                box.transform.translation = [0, 3, -2]
                 
                 
                 // box that obeys physics
                 let physicsBox = ModelEntity(mesh: .generateBox(size: 0.3), materials: [SimpleMaterial(color: .red, isMetallic: false)])
                 let collisionBox = ShapeResource.generateBox(size: SIMD3(x: 0.3, y: 0.3, z: 0.3))
                 
-                var physics = PhysicsBodyComponent(shapes: [collisionBox], density: 9_999_999)
+                var physics = PhysicsBodyComponent(massProperties: .default, material: nil, mode: .dynamic)
                 
-                // turn gravity on explicitly
+                // setup physics box
                 physics.isAffectedByGravity = true
-                
                 physicsBox.components.set(CollisionComponent(shapes: [collisionBox]))
-                
                 physicsBox.components.set(physics)
-                
-                physicsBox.transform.translation = [1, 1, -3]
+                physicsBox.transform.translation = [1, 3, -3]
                 
                 immersiveContentEntity.addChild(physicsBox)
                 
+                // setup the respawnPlane
+                
                 // https://developer.apple.com/documentation/uikit/uicollisionbehavior/1624828-collisiondelegate?changes=_2_8
                 
-                let respawnPlane = ModelEntity(mesh: .generatePlane(width: 10, depth: 10))
+                let respawnPlane = ModelEntity(mesh: .generatePlane(width: 100, depth: 100))
                 respawnPlane.transform.translation = [0, -34, 0]
+                
+                // Add collision component to the plane
+                let collisionPlaneBox = ShapeResource.generateBox(size: SIMD3(x: 100, y: 0.1, z: 100))
+                respawnPlane.components.set(CollisionComponent(shapes: [collisionPlaneBox]))
+
                 
                 immersiveContentEntity.addChild(respawnPlane)
                 
-                           
+                immersiveContentEntity.scene?.subscribe(to:
+                                                            CollisionEvents.Began.self, on: physicsBox, {  event in
+                    
+                    print("Collision began between \(event.entityA.name) and \(event.entityB.name)")
+                    physicsBox.transform.translation = [1, 3, -3]
+                })
             }
         }
     }
