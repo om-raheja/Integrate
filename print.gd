3ba3f26a40ee767811f139d52fd8b271702fe1d6
Om Raheja <omr@rslp.org>
2024-07-13 14:39:23 -0400

    added working collision detection

--- print.gd
+++ print.gd
@@ -3,6 +3,9 @@ extends RigidBody3D
 var last_velocity = Vector3.ZERO
 
 func _ready():
+	self.contact_monitor = true
+	self.max_contacts_reported = 5
+	#self.contacts_reported = 5
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 	set_collision_layer(1)
@@ -11,17 +14,17 @@ func _ready():
 func _integrate_forces(state):
 	# Capture the velocity just before collision
 	last_velocity = state.get_linear_velocity()
-	print("no", last_velocity)
+	#print("no", last_velocity)
 	
 	# Check for collisions
-	print(state.get_contact_count())
+	print(get_contact_count())
 
 func _on_body_entered(body):
 	print("wut")
 	if body is StaticBody3D:
 		print("Box has hit the floor")
-		var window_dialog = get_node("/root/Node3D/Control/WindowDialog")
-		var label = window_dialog.get_node("Label")
+		var window_dialog = get_node("/Control/Window")
+		var label = get_node("/Control/Window/Label")
 		
 		# Show the window dialog and set the text
 		window_dialog.show()
